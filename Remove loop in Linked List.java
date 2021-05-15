cf379529cb574007672823b997a297240ed300c8
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2021-05-15 08:11:42 +0530

    New Solution added

--- Remove loop in Linked List.java
+++ Remove loop in Linked List.java
@@ -12,3 +12,47 @@ class Solution{
         if(head != null) head.next = null;
     }
 }
+
+
+
+//-----------------------------------------0(n) + 0(1) space ------------------------------------
+
+class Solution{
+    public static void removeLoop(Node node){
+        if (node == null || node.next == null)
+            return;
+ 
+        Node slow = node, fast = node;
+ 
+        slow = slow.next;
+        fast = fast.next.next;
+ 
+        while (fast != null && fast.next != null) {
+            if (slow == fast)
+                break;
+ 
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+ 
+        if (slow == fast) {
+            slow = node;
+            while (slow.next != fast.next) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+ 
+            if(fast != node){
+                fast.next = null;  
+            }else{
+                fast = fast.next;
+                while(fast.next != slow){
+                    fast = fast.next;
+                }
+                fast.next = null;
+            }
+        }
+        
+        
+    }
+}
